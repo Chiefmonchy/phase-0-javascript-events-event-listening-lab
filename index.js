28e1ae756d375494088f50b9e34c2d19e8579152
Brady Maradiaga <chiefmonchyy@gmail.com>
2023-05-18 21:13:11 -0400

    completed first events lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function clickAlert() {
+    alert('I was clicked!');
 }
+
+function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+
+
+}
+
+addingEventListener();
